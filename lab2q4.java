e102cf7665793c8fda36a63a55a9dff5160bdd40
Asadullah Shaikh <asadullahsaeed12345@gmail.com>
2026-03-08 21:48:43 +0500

    Update lab2q4.java

--- lab2q4.java
+++ lab2q4.java
@@ -1,29 +1,37 @@
 public class lab2q4 {
     public static void main(String[] args) {
-        String name = "Asad";
+        // String name = "Asad";
+        // int age = 20;
+        // double marks = 85.5;
+        // char grade = 'A';
+        // boolean result = true;
+
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(marks);
+        // System.out.println(grade);
+
         int[] numbers = { 10, 20, 30 };
+        for(int i:numbers){
+            System.out.println("The number "+(i/10)" is: "+i);
+        }
         
         class Student {
             String name;
             int age;
+
+        public void display() {
+            System.out.println("The Name of student is: "+name);
+            System.out.println("The Age of student is: "+age);
             }
+        }
             Student s1 = new Student();
             s1.name="Asif";
             s1.age=18;
-            
-            System.out.println(name);
-            System.out.println(s1.name);
-            System.out.println(s1.age);
+            s1.display();
         
-    }
+        }
     }
 
-// int age = 20;
-// double marks = 85.5;
-// char grade = 'A';
-// boolean result = true;
 
-// System.out.println(age);
-// System.out.println(marks);
-// System.out.println(grade);
 // System.out.println(result);
